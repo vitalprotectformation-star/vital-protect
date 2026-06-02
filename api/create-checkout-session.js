11cd97ed88a6446db0db69fe3b3812a37bf4e604
vitalprotectformation-star <vitalprotectformation@gmail.com>
2026-06-02 13:18:17 +0200

    Add files via upload

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -319,6 +319,7 @@ export default async function handler(req, res) {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
+      allow_promotion_codes: true,
       customer_email: email,
       line_items: [
         {
--- api/create-trainer-checkout.js
+++ api/create-trainer-checkout.js
@@ -495,6 +495,7 @@ export default async function handler(req, res) {
     const checkoutSession = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
+      allow_promotion_codes: true,
       customer_email: cleanEmail,
 
       line_items: [
